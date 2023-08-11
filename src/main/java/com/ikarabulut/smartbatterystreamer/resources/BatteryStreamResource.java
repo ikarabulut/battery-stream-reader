100a90262d151b70a102e8c1c93e9b1f974c3c58
Ismail Karabulut <i.karabulut93@gmail.com>
2023-08-10 20:08:17 -0500

    update resource to produce to kafka topic

--- src/main/java/com/ikarabulut/smartbatterystreamer/resources/BatteryStreamResource.java
+++ src/main/java/com/ikarabulut/smartbatterystreamer/resources/BatteryStreamResource.java
@@ -1,15 +1,21 @@
 package com.ikarabulut.smartbatterystreamer.resources;
 
+import com.google.common.collect.ImmutableMap;
 import com.smartbatterystreamer.avro.BatteryEvent;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.time.Instant;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 import static jakarta.ws.rs.core.MediaType.APPLICATION_OCTET_STREAM;
@@ -29,11 +35,23 @@ public class BatteryStreamResource {
     @Consumes({APPLICATION_OCTET_STREAM, APPLICATION_JSON})
     @Produces(APPLICATION_JSON)
     public Response send(@PathParam("uuid") String uuid, @Context HttpServletRequest request)
-            throws IOException {
+            throws IOException, ExecutionException, InterruptedException {
         ByteBuffer body = ByteBuffer.wrap(toByteArray(request.getInputStream()));
         BatteryEvent payload = new BatteryEvent(uuid, Instant.now().toEpochMilli(), body);
 
-        return Response.ok().entity(payload).build();
+        ProducerRecord record = new ProducerRecord(topic, uuid, payload);
+        Future<RecordMetadata> metadata = producer.send(record);
+
+        return Response.ok().entity(serialize(metadata.get())).build();
+    }
+
+    protected Map<String, Object> serialize(RecordMetadata metadata) {
+        return ImmutableMap.<String, Object>builder()
+                .put("offset", metadata.offset())
+                .put("partition", metadata.partition())
+                .put("topic", metadata.topic())
+                .put("timestamp", metadata.timestamp())
+                .build();
     }
 }
 
